9deb1bcf394a64470bc84138dba79addd834b826
Martin Suesskraut <martin.suesskraut@gmail.com>
2017-11-26 19:44:47 +0100

    implemented selection of next move

--- src/bruteforce.rs
+++ src/bruteforce.rs
@@ -1,5 +1,7 @@
 use field::{Field, Board};
 
+use rand::random;
+
 fn is_valid_pair_rule(board: &Board, x: usize, y: usize) -> bool {
     let current = board.get(x, y);
     let size = board.get_size();
@@ -146,9 +148,73 @@ pub fn calc_possible_moves(board: &mut Board) -> Vec<PossibleMove> {
     result
 }
 
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Move {
+    field: Field,
+    x: usize,
+    y: usize,
+}
+
+pub fn select_next_move(possible_moves: &[PossibleMove]) -> Option<Move> {
+    if possible_moves.is_empty() || possible_moves.contains(&PossibleMove::NoMove) {
+        None
+    } else {
+        let single_options = possible_moves
+            .iter()
+            .filter(|e| match *(*e) {
+                        PossibleMove::OneMove(_, _, _) => true,
+                        _ => false,
+                    })
+            .collect::<Vec<&PossibleMove>>();
+
+        if !single_options.is_empty() {
+            match *single_options[random::<usize>() % single_options.len()] {
+                PossibleMove::OneMove(x, y, field) => Some(Move { field, x, y }),
+                _ => unreachable!(),
+            }
+        } else {
+            match possible_moves[random::<usize>() % possible_moves.len()] {
+                PossibleMove::TwoMoves(x, y) => {
+                    let field = if random::<bool>() { Field::X } else { Field::O };
+                    Some(Move { field, x, y })
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn select_none_if_one_impossible_move_in_list() {
+        assert_eq!(None,
+                   select_next_move(&[PossibleMove::OneMove(1, 1, Field::X),
+                                      PossibleMove::TwoMoves(2, 2),
+                                      PossibleMove::NoMove]));
+    }
+
+    #[test]
+    fn select_random_one_move_if_list_of_possible_moves_contains_one_move() {
+        let possible_moves = vec![PossibleMove::OneMove(1, 1, Field::X),
+                                  PossibleMove::OneMove(2, 1, Field::O),
+                                  PossibleMove::TwoMoves(2, 2)];
+        let next_move = select_next_move(&possible_moves).unwrap();
+        assert!(possible_moves.contains(&PossibleMove::OneMove(next_move.x,
+                                                               next_move.y,
+                                                               next_move.field)));
+    }
+
+    #[test]
+    fn select_random_two_move_if_list_of_possible_moves_contains_only_two_moves() {
+        let possible_moves = vec![PossibleMove::TwoMoves(1, 1),
+                                  PossibleMove::TwoMoves(2, 1),
+                                  PossibleMove::TwoMoves(2, 2)];
+        let next_move = select_next_move(&possible_moves).unwrap();
+        assert!(possible_moves.contains(&PossibleMove::TwoMoves(next_move.x, next_move.y)));
+    }
 
     #[test]
     fn get_possible_moves_for_all_empty_fields() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(stmt_expr_attributes)]
 
+extern crate rand;
+
 #[macro_use]
 pub mod field;
 pub mod bruteforce;
